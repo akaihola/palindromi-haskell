522ceeb62c6bc3542ed8a2adec554fbda031f5f5
jasaarim <j.saarimaki@gmail.com>
2015-05-03 20:36:42 +0300

    Minor cleaning: shortening long lines, removing redundant returns, defining a couple of helper functions

--- src/TriePalindrome.hs
+++ src/TriePalindrome.hs
@@ -1,13 +1,14 @@
 module TriePalindrome (main, readTrieFromFile) where
 
 import Prelude hiding (filter)
-import Data.ListTrie.Patricia.Set (empty, filter, insert, lookupPrefix, toList, TrieSet)
+import Data.ListTrie.Patricia.Set (empty, filter, insert, lookupPrefix, 
+                                   toList, TrieSet)
 import qualified Data.ListTrie.Patricia.Set as S (map)
 import Data.List (intercalate, isPrefixOf)
 import Data.Map (Map)
 import GHC.IO.Handle.Types (Handle)
 
-import System.IO          (hGetLine, hIsEOF, openFile, IOMode(ReadMode), stdin)
+import System.IO (hGetLine, hIsEOF, openFile, IOMode(ReadMode), stdin)
 import System.Environment (getArgs)
 
 import Debug.Trace (trace)
@@ -15,23 +16,25 @@ import Debug.Trace (trace)
 
 type Wordbook = TrieSet Map Char
 
+
 readIntoTrie :: Handle -> Int -> Wordbook -> IO Wordbook
-readIntoTrie handle linenum t = do eof <- hIsEOF handle
-                                   if eof then return t
-                                   else do line <- hGetLine handle
-                                           (readIntoTrie handle $! linenum + 1) $! insert line t
+readIntoTrie handle linenum t = 
+  do 
+    eof <- hIsEOF handle
+    if eof 
+      then return t
+      else do line <- hGetLine handle
+              (readIntoTrie handle $! linenum + 1) $! insert line t
+
 
 readTrieFromStdin :: IO (Wordbook)
-readTrieFromStdin =
-  readIntoTrie handle 0 empty >>= return
-      where handle = stdin
+readTrieFromStdin = readIntoTrie stdin 0 empty
 
 
 readTrieFromFile :: String -> IO (Wordbook)
 readTrieFromFile path = do
   handle <- openFile path ReadMode
-  t <- readIntoTrie handle 0 empty
-  return t
+  readIntoTrie handle 0 empty
 
 
 isPalindrome :: String -> Bool
@@ -41,32 +44,38 @@ isPalindrome bs = reverse bs == bs
 type Match = (String, String)
 type Palindrome = [String]
 
+
 getLongerMatches :: String -> Wordbook -> [Match]
-getLongerMatches prefix dictionary = [(match, drop (length prefix) match)
-                                     | match <- toList (lookupPrefix prefix dictionary),
-                                                match /= prefix]
+getLongerMatches prefix dictionary = 
+  [(match, drop (length prefix) match)
+  | match <- toList (lookupPrefix prefix dictionary)
+  , match /= prefix]
+
 
 getShorterMatches :: String -> Wordbook -> [Match]
-getShorterMatches prefix dictionary = [(match, drop (length match) prefix)
-                                      | match <- toList (filter ((flip isPrefixOf) prefix) dictionary)]
-
+getShorterMatches prefix dictionary = 
+  [(match, drop (length match) prefix)
+  | match <- toList (filter ((flip isPrefixOf) prefix) dictionary)]
+  
+  
 findPalindromes :: Int -> String -> Wordbook -> Wordbook -> [Palindrome]
 findPalindromes 0 _ _ _ = []
 findPalindromes n prefix dictionary reverseDictionary 
     = final ++ longer ++ shorter
-    where final :: [Palindrome]
-          final | isPalindrome prefix = [[]]
-                | otherwise = []
-          longerMatches = getLongerMatches prefix dictionary
-          shorterMatches = getShorterMatches prefix dictionary
-          longer = [(match:(reverse $ map reverse restBackwards))
-                        | (match, overflow) <- longerMatches,
-                          restBackwards <- findPalindromes (n - 1) overflow reverseDictionary dictionary,
-                          not (elem match (reverse $ map reverse restBackwards))]
-          shorter = [(match:rest)
-                        | (match, underflow) <- shorterMatches,
-                          rest <- findPalindromes (n - 1) underflow dictionary reverseDictionary,
-                          not (elem match rest)]
+    where 
+      final :: [Palindrome]
+      final | isPalindrome prefix = [[]]
+            | otherwise = []
+      longer = [(match:(reverse2d restBackwards))
+               | (match, overflow) <- getLongerMatches prefix dictionary
+               , restBackwards <- loop overflow reverseDictionary dictionary
+               , not (elem match (reverse2d restBackwards))]
+      shorter = [(match:rest)
+                | (match, underflow) <- getShorterMatches prefix dictionary
+                , rest <- loop underflow dictionary reverseDictionary
+                , not (elem match rest)]
+      reverse2d = reverse . map reverse
+      loop = findPalindromes (n - 1)
 
 
 main :: IO ()
