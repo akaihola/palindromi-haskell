3048a693671e64e9e5a86101121afd4c06776211
Antti Kaihola <antti.kaihola@eniram.fi>
2015-05-04 21:44:41 +0300

    Now reads any text file and converts to a vocabulary

--- src/WordTrie.hs
+++ src/WordTrie.hs
@@ -4,39 +4,51 @@ module WordTrie (getLongerMatches,
                  readTrieFromStdin,
                  Wordbook) where
 
-import Prelude hiding (filter)
+import Data.Char (isAlpha, toLower)
 import Data.List (isPrefixOf)
-import Data.ListTrie.Patricia.Set (empty, filter, insert, lookupPrefix,
+import Data.ListTrie.Patricia.Set (empty, insert, lookupPrefix,
                                    toList, TrieSet)
+import qualified Data.Text as T (dropWhile, dropWhileEnd, filter, pack, Text, toLower, unpack)
+import qualified Data.ListTrie.Patricia.Set as Set (filter)
 import Data.Map (Map)
 import GHC.IO.Handle.Types (Handle)
-import System.IO (hGetLine, hIsEOF, openFile, IOMode(ReadMode), stdin)
+import System.IO (hGetContents, hGetLine, hIsEOF, openFile, IOMode(ReadMode), stdin)
 
 
 type Wordbook = TrieSet Map Char
-readIntoTrie :: Handle -> Wordbook -> IO Wordbook
-readIntoTrie handle t =
-  do
-    eof <- hIsEOF handle
-    if eof
-      then return t
-      else do line <- hGetLine handle
-              (readIntoTrie handle) $! insert line t
+readIntoTrie :: [String] -> Wordbook -> IO Wordbook
+readIntoTrie [] wordbook = do
+  return wordbook
+readIntoTrie (x:xs) wordbook | null word = readIntoTrie xs wordbook
+                             | otherwise = readIntoTrie xs $ insert word wordbook
+    where word = normalizeWord x
 
 readTrieFromStdin :: IO (Wordbook)
-readTrieFromStdin = readIntoTrie stdin empty
+readTrieFromStdin = do
+  content <- hGetContents stdin
+  let tokens = words content
+  readIntoTrie tokens empty
 
+
+strip :: T.Text -> T.Text
+strip = T.dropWhileEnd (not . isAlpha) . T.dropWhile (not . isAlpha)
+clean :: T.Text -> T.Text
+clean = T.filter isAlpha
+
+normalizeWord = T.unpack . clean . T.toLower . strip . T.pack
 
 readTrieFromFile :: String -> IO (Wordbook)
 readTrieFromFile path = do
   handle <- openFile path ReadMode
-  readIntoTrie handle empty
+  content <- hGetContents handle
+  let tokens = words content
+  readIntoTrie tokens empty
 
 
 type Match = (String, String)
 
 lookupIsPrefixTo :: String -> Wordbook -> Wordbook
-lookupIsPrefixTo word = filter ((flip isPrefixOf) word)
+lookupIsPrefixTo word = Set.filter ((flip isPrefixOf) word)
 
 getLongerMatches :: String -> Wordbook -> [Match]
 getLongerMatches prefix dictionary =
